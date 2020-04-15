1ae4e8636a4135007fae394fb06edb55c3134515
richierich25 <richierich25@gmail.com>
2020-04-15 09:56:21 +0530

    added new modules for intermediate JS

---
+++ JS/IntermediateJS/modules/WorkAround/workAround.js
@@ -0,0 +1,14 @@
+const employee = require('./employee.js');
+
+function getEmployeeInformation(inputSalary) {
+  employee.salary = inputSalary;
+  console.log('Cadre: ' + employee.getCadre());
+  console.log('Tax: ' + employee.calculateTax());
+  console.log('Benefits: ' + employee.getBenefits());
+  console.log('Bonus: ' + employee.calculateBonus());
+  console.log('Reimbursement Eligibility: ' + employee.reimbursementEligibility() + '\n');
+}
+
+getEmployeeInformation(10000);
+getEmployeeInformation(50000);
+getEmployeeInformation(100000);
